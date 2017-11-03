2533f754f3f3f6b874f3661c4aab6850f6d5afac
Li-Hsuan Lung <lihsuan@8thlight.com>
2017-11-03 16:12:12 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Li-Hsuan Lung on 11/3/17.
+//  Copyright © 2017 Li-Hsuan Lung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
